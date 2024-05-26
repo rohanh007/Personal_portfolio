7d30903c48d5031f9b45b6b350a60ea4bc791b2c
rohanh007 <77897898+rohanh007@users.noreply.github.com>
2024-05-26 16:13:55 +0530

    open close menu

--- src/components/Mynavabar.js
+++ src/components/Mynavabar.js
@@ -17,15 +17,17 @@ const MyNavbar = () => {
 
       {isOpen &&
            (
-            <div className="menu_box">
-             <div className="close_cross" onClick={toggleNavbar}><TfiClose color="white" size={40}/></div>
-            <ul className='navbar-links' id="navbar-links">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-        </div>
+          <div className="menu_box">
+            <div className="close_cross" onClick={toggleNavbar}><TfiClose color="white" size={40} /></div>
+            <div className="mennu_items_box">
+              <ul className='navbar-links' id="navbar-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
            )
       }
         
