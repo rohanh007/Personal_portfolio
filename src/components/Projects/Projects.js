a1e484a04649d6291ca11c1520ce93c046a9047a
rohanh007 <77897898+rohanh007@users.noreply.github.com>
2024-06-01 14:50:01 +0530

    add links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -29,11 +29,11 @@ const  Projects=()=>{
               </figure>
               <div className="article-body">
                 <h2>Swiggy Clone</h2>
-                <p>
-                  Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
-                </p>
+                <p className="project_txt_height">
+This project is a Swiggy clone, crafted with ReactJS and Redux Toolkit for state management. CSS styling elevate the user interface, while the Swiggy API fuels real-time restaurant and menu data. Developers delve into React best practices, Redux Toolkit, API integration, and responsive design.
+                 </p>
                 <div class="button-wrapper"  >
-                  <a href="https://github.com/your-github-project" target="_blank" class="github-button" onMouseOver={(e) => { e.stopPropagation() }}>GitHub</a>
+                  <a href="https://github.com/rohanh007/Namaste-React/tree/main/Time_for_test" target="_blank" class="github-button" onMouseOver={(e) => { e.stopPropagation() }}>GitHub</a>
                   <a href="https://yourproject.com" target="_blank" class="demo-button"  onMouseOver={(e) => { e.preventDefault() }}>Live Demo</a>
                 </div>
               </div>
@@ -45,12 +45,12 @@ const  Projects=()=>{
                 <img src={cooking_blog} alt="" />
               </figure>
               <div className="article-body">
-                <h2>This is some title</h2>
-                <p>
-                  Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
+                <h2>Cooking Blogs Nodejs</h2>
+                <p className="project_txt_height">
+                This project is a dynamic and user-friendly Cooking Blog powered by Node.js, Express.js, and MongoDB. It allows users to create, share, and discover delicious recipes in a seamless and interactive environment.
                 </p>
                 <div class="button-wrapper">
-                  <a href="https://github.com/your-github-project" target="_blank" class="github-button">GitHub</a>
+                  <a href="https://github.com/rohanh007/Cooking_Recipe_Blogs_nodejs_mongodb_expressjs" target="_blank" class="github-button">GitHub</a>
                   <a href="https://yourproject.com" target="_blank" class="demo-button">Live Demo</a>
                 </div>
               </div>
@@ -62,12 +62,12 @@ const  Projects=()=>{
                 <img src={technews} alt="" />
               </figure>
               <div className="article-body">
-                <h2>This is some title</h2>
-                <p>
-                  Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
-                </p>
+                <h2>TechNews</h2>
+                <p className="project_txt_height">
+                build a simple TechNews website/app using Node.js, Express, EJS and we'll be also using some dependencies such as AXIOS, Body-Parser and Nodemon,also use news api.
+                 </p>
                 <div class="button-wrapper">
-                  <a href="https://github.com/your-github-project" target="_blank" class="github-button">GitHub</a>
+                  <a href="https://github.com/rohanh007/nodejs-technews-website.github.io" target="_blank" class="github-button">GitHub</a>
                   <a href="https://yourproject.com" target="_blank" class="demo-button">Live Demo</a>
                 </div>
               </div>
--- src/components/Socialiconfixed.js
+++ src/components/Socialiconfixed.js
@@ -1,15 +1,19 @@
-import { FaGithub, FaInstagram, FaLinkedinIn, FaCodepen } from "react-icons/fa";
+import { FaGithub, FaInstagram, FaLinkedinIn} from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
 
 
 const Socialiconfixed = () => {
     return (
         
             <div className="social-menu">
+            {/* <p className="web_white_color" style={{ fontSize: '20px' }}>
+                Feel free to <span className="web-text-color">connect </span>with me
+            </p> */}
                 <ul>
-                    <li><a href="https://github.com/sanketbodke" target="_blank"><FaGithub className="fab" /></a></li>
-                    <li><a href="https://www.instagram.com/imsanketbodke/" target="_blank"><FaInstagram className="fab" /></a></li>
-                    <li><a href="https://www.linkedin.com/in/sanket-bodake-995b5b205/" target="_blank"><FaLinkedinIn className="fab" /></a></li>
-                    <li><a href="https://codepen.io/sanketbodke" target="_blank"><FaCodepen className="fab" /></a></li>
+                    <li><a href="https://github.com/rohanh007" target="_blank"><FaGithub className="fab" /></a></li>
+                    {/* <li><a href="https://www.instagram.com/imsanketbodke/" target="_blank"><FaInstagram className="fab" /></a></li> */}
+                    <li><a href="https://www.linkedin.com/in/rohan-hoval-1163211ab/" target="_blank"><FaLinkedinIn className="fab" /></a></li>
+                    <li><a href="https://leetcode.com/u/rohanhoval/" target="_blank"><SiLeetcode className="fab" /></a></li>
                 </ul>
             </div>
        
